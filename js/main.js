6576245b0cf2a877fe83312552da6988f4bb6edb
kevinwen0130-wq <kevinwen0130@gmail.com>
2026-07-13 15:08:18 +0800

    建立 V4 模組架構，加入 messages.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-console.log("Apple OS Simulator V4 啟動成功！");
+document.addEventListener("DOMContentLoaded", () => {
+    document.title = "Apple OS Simulator V4";
+    console.log("Apple OS Simulator V4 啟動成功！");
+});
