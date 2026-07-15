bd2f50a89767d3e8c069d64919e7735a00e0dbc6
kevinwen0130-wq <kevinwen0130@gmail.com>
2026-07-15 17:29:03 +0800

    Safari 支援開啟真實網站

--- js/safari.js
+++ js/safari.js
@@ -1,8 +1,25 @@
 window.safariData = {
-  title: "Safari",
-  url: "https://www.apple.com",
   pageTitle: "Apple",
-  content: "歡迎使用 Safari 模擬器"
+  url: "https://www.apple.com",
+  content: "歡迎使用 Safari 模擬器",
+
+  pages: {
+    apple: {
+      title: "Apple",
+      url: "https://www.apple.com",
+      content: "🍎 歡迎來到 Apple 官網"
+    },
+    github: {
+      title: "GitHub",
+      url: "https://github.com",
+      content: "🐙 GitHub 程式碼平台"
+    },
+    wikipedia: {
+      title: "Wikipedia",
+      url: "https://wikipedia.org",
+      content: "📚 自由的百科全書"
+    }
+  }
 };
 
 console.log("safari.js 載入成功！");
